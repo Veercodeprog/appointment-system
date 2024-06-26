4c5825cf86dd2ae999e53fce67447fc0b607c4b4
veerpratapsinghtomar <Veerpratap945050@gmail.com>
2024-06-26 23:21:01 +0530

    env added

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-const BASE_URL = "http://localhost:3500";
+// const BASE_URL = "http://localhost:3500";
+const BASE_URL = process.env.BASE_URL;
 export default axios.create({
   baseURL: BASE_URL,
 });
